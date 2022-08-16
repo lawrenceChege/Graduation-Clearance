02cff3f14dd91d36e3d81e1e6061534b4c425e8e
Your Name <you@example.com>
2022-08-16 13:35:03 +0300

    finance models

--- Finance/models.py
+++ Finance/models.py
@@ -1,3 +1,29 @@
+from tkinter import CASCADE
 from django.db import models
 
-# Create your models here.
+from base.models import BaseModel, GenericBaseModel, State
+
+class Fee(GenericBaseModel):
+    """
+	Defines the different fees: School fee, Graduation fee, library fee
+	"""
+    amount = models.DecimalField(default=0.00, max_digits=25)
+    status = models.ForeignKey(State, on_delete=models.CASCADE)
+
+class StudentFee(BaseModel):
+    """
+    Defines fees for students
+    """
+    fee = models.ForeignKey(Fee, on_delete=models.CASCADE)
+    amount_paid = models.DecimalField(default=0.00, max_digits=25)
+    balance = models.DecimalField(default=0.00, max_digits=25)
+
+class StudentFeePayment(BaseModel):
+    """
+    Defines individual payment
+    """
+    student_fee = models.ForeignKey(StudentFee, on_delete=CASCADE)
+    amount = models.DecimalField(default=0.00, max_digits=25)
+    receipt = models.TextField(max_length=50, null=True, blank=True)
+    
+
